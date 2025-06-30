b07dde016125d6d814de13a7557d790f27278a6e
leiwangamy <leiwangamy@gmail.com>
2025-06-30 06:36:51 +0000

    add first pictures to cart

--- public/app.js
+++ public/app.js
@@ -88,6 +88,7 @@ fetch("/api/products")
           id: product.id,
           name: product.name,
           price: product.price,
+          image: product.images?.[0] || "", 
         });
       };
       card.appendChild(button);
